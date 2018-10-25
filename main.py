46129608453f3a237a8a89d34338e8ded434e101
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2018-10-25 17:51:31 -0300

    Add a input to the file
    Now you can do anything you want with this input

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ class PurpleBananaMonkey(self, monkey_type = "Chimpanzee", color = "Purple"):
 
 def main():
     print("Let's get started!")
+    user_input = input("Type something");
 
 if __name__ == "__main__":
     main()
+
